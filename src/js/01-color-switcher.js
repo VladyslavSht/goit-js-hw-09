01f0d3ff6f0afe7145b3814fbfd54f78b8e88ae0
Vladyslav Shtompel <vladyslav.shtompel@gmail.com>
2022-04-15 14:59:33 +0300

    first task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+  };
 
+refs.startBtn.addEventListener('click', handleStart);
+refs.stopBtn.addEventListener('click', handleStop)
+
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function handleStart() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    refs.startBtn.disabled = true;
+}
+
+function handleStop() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+}
